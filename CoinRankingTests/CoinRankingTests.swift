5f465ec1f3beb82e0e7c5aebd8c5cc0132a696f7
Kelvin Ofula <kelvinofula19@gmail.com>
2025-04-30 14:08:38 +0300

    Initial Commit

---
+++ CoinRankingTests/CoinRankingTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRankingTests.swift
+//  CoinRankingTests
+//
+//  Created by Kelvin Ofula on 4/30/25.
+//
+
+import Testing
+@testable import CoinRanking
+
+struct CoinRankingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
